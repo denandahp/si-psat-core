51584b11ecbe26bc0017795cdd5e840451d0f70e
farchan <farchan.hakim.r@mail.ugm.ac.id>
2021-12-05 12:45:29 +0800

    add sertif pl

--- models/sppb_psat_audit.js
+++ models/sppb_psat_audit.js
@@ -21,7 +21,8 @@ class PsatPlPerubahanModel {
         try {
             let penunjukan_tim_audit;
             let data_penunjukan_tim_audit = [
-                data.id_pengajuan, data.tanggal_penugasan, data.surat_tugas];
+                data.id_pengajuan, data.tanggal_penugasan, data.surat_tugas
+            ];
             console.log(data)
             penunjukan_tim_audit = await pool.query(
                 format('CALL ' + proc_tim_audit + `(%L,'{${data.lead_auditor}}' , '{${data.tim_auditor}}')`, data_penunjukan_tim_audit));
@@ -35,15 +36,15 @@ class PsatPlPerubahanModel {
 
     async audit_dokumen(data) {
         try {
-            let audit_dokumen, data_audit_dokumen, audit_lapang, data_audit_lapang ;
-            if (data.proses == 'CLEAR'){
+            let audit_dokumen, data_audit_dokumen, audit_lapang, data_audit_lapang;
+            if (data.proses == 'CLEAR') {
                 data_audit_dokumen = [data.id_pengajuan, data.id_tim_audit, data.proses];
                 audit_dokumen = await pool.query(format('CALL ' + proc_audit_doc + ' (%L)', data_audit_dokumen));
                 console.log("OKE")
 
                 data_audit_lapang = [data.id_pengajuan, data.id_tim_audit, 'REVIEW'];
                 audit_lapang = await pool.query(format('CALL ' + proc_audit_lapang + ' (%L)', data_audit_lapang));
-            }else{
+            } else {
                 data_audit_dokumen = [
                     data.id_pengajuan, data.id_tim_audit, data.proses, data.hasil_audit, JSON.stringify(data.keterangan)
                 ];
@@ -59,14 +60,14 @@ class PsatPlPerubahanModel {
 
     async audit_lapang(data) {
         try {
-            let audit_lapang, data_audit_lapang,  sidang_komtek, data_sidang_komtek ;
-            if (data.proses == 'CLEAR'){
+            let audit_lapang, data_audit_lapang, sidang_komtek, data_sidang_komtek;
+            if (data.proses == 'CLEAR') {
                 data_audit_lapang = [data.id_pengajuan, data.id_tim_audit, data.proses];
                 audit_lapang = await pool.query(format('CALL ' + proc_audit_lapang + ' (%L)', data_audit_lapang));
 
                 data_sidang_komtek = [data.id_pengajuan, data.id_tim_komtek, 'REVIEW'];
                 sidang_komtek = await pool.query(format('CALL ' + proc_sidang_komtek + ' (%L)', data_sidang_komtek));
-            }else{
+            } else {
                 data_audit_lapang = [data.id_pengajuan, data.id_tim_audit, data.proses, data.hasil_audit, JSON.stringify(data.keterangan)];
                 audit_lapang = await pool.query(format('CALL ' + proc_audit_lapang + ' (%L)', data_audit_lapang));
             }
@@ -92,7 +93,7 @@ class PsatPlPerubahanModel {
 
     async audit_rekomendasi(data) {
         try {
-            let sidang_komtek, data_sidang_komtek ;
+            let sidang_komtek, data_sidang_komtek;
             data_sidang_komtek = [data.id_pengajuan, data.id_tim_komtek, data.proses, data.hasil_audit, JSON.stringify(data.keterangan)];
             sidang_komtek = await pool.query(format('CALL ' + proc_sidang_komtek + ' (%L)', data_sidang_komtek));
             return { status: '200', ketarangan: `${data.proses} SIDANG KOMTEK `, data: sidang_komtek.rows[0] };
@@ -107,17 +108,17 @@ class PsatPlPerubahanModel {
             let history;
             history = await pool.query(
                 ' SELECT id_pengajuan, id_audit, status_proses, created, update' +
-                ' id_tim_audit, tanggal_penugasan_tim_audit, surat_tugas_tim_audit, lead_auditor, tim_auditor' + 
+                ' id_tim_audit, tanggal_penugasan_tim_audit, surat_tugas_tim_audit, lead_auditor, tim_auditor' +
                 ' id_tim_komtek, tanggal_penugasan_tim_komtek, surat_tugas_tim_komtek, lead_komtek, tim_komtek' +
-                ' id_audit_dokumen, mulai_audit_dokumen, tenggat_audit_dokumen, selesai_audit_dokumen, mulai_perbaikan_audit_dokumen'+
+                ' id_audit_dokumen, mulai_audit_dokumen, tenggat_audit_dokumen, selesai_audit_dokumen, mulai_perbaikan_audit_dokumen' +
                 ' tenggat_perbaikan_audit_dokumen, selesai_perbaikan_audit_dokumen, keterangan_audit_dokumen, hasil_audit_dokumen,' +
-                ' id_audit_lapang, mulai_audit_lapang, tenggat_audit_lapang, selesai_audit_lapang, mulai_perbaikan_audit_lapang'+
+                ' id_audit_lapang, mulai_audit_lapang, tenggat_audit_lapang, selesai_audit_lapang, mulai_perbaikan_audit_lapang' +
                 ' tenggat_perbaikan_audit_lapang, selesai_perbaikan_audit_lapang, keterangan_audit_lapang, hasil_audit_lapang,' +
-                ' id_sidang_komtek, mulai_sidang_komtek, tenggat_sidang_komtek, selesai_sidang_komtek, mulai_perbaikan_sidang_komtek'+
-                ' tenggat_perbaikan_sidang_komtek, selesai_perbaikan_sidang_komtek, keterangan_sidang_komtek, hasil_sidang_komtek FROM' + 
+                ' id_sidang_komtek, mulai_sidang_komtek, tenggat_sidang_komtek, selesai_sidang_komtek, mulai_perbaikan_sidang_komtek' +
+                ' tenggat_perbaikan_sidang_komtek, selesai_perbaikan_sidang_komtek, keterangan_sidang_komtek, hasil_sidang_komtek FROM' +
                 db_history_audit + ' WHERE id_pengajuan=$1 ORDER BY created ASC', [id_pengajuan])
             check_query.check_queryset(history);
-            return { status: '200', keterangan: `History Audit id Pengajuan ${id_pengajuan}` , data: history.rows };
+            return { status: '200', keterangan: `History Audit id Pengajuan ${id_pengajuan}`, data: history.rows };
         } catch (ex) {
             console.log('Enek seng salah iki ' + ex);
             return { status: '400', Error: "" + ex };
