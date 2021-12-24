69cd9bf38af6a31ee098c15a7d0d81c58f6fd715
farchan <farchan.hakim.r@mail.ugm.ac.id>
2021-12-24 15:34:35 +0800

    edit

--- controllers/utils/generatePdf.js
+++ controllers/utils/generatePdf.js
@@ -81,108 +81,108 @@ class generatePdfController {
 
     async psat_pl(req, res, next) {
         let callback = async() => {
-            // try {
-            const def = req.body
-            const param = req.params
-            let sertifikat_pl = await sppb_pl_view.view_sertifikat(param)
-            let unit_produksi = await sppb_pl_view.view_unitproduksi(param)
-            let berlaku_sampai = new Date(new Date().setFullYear(new Date().getFullYear() + 5))
+            try {
+                const def = req.body
+                const param = req.params
+                let sertifikat_pl = await sppb_pl_view.view_sertifikat(param)
+                let unit_produksi = await sppb_pl_view.view_unitproduksi(param)
+                let berlaku_sampai = new Date(new Date().setFullYear(new Date().getFullYear() + 5))
 
-            let data = {
+                let data = {
 
-                "pbumku": "pbumku",
-                "nama_psat": "nama_psat",
-                "jenis_psat": sertifikat_pl.jenis_psat,
-                "nama_dagang": sertifikat_pl.nama_dagang,
-                "izin_psat_pl": " ",
-                "no_izin_psat_pl": def.no_izin_psat_pl,
-                "berlaku_sampai": berlaku_sampai,
-                "nama_latin": sertifikat_pl.nama_latin,
-                "negara_asal": sertifikat_pl.negara_asal,
-                "nama_merk": sertifikat_pl.nama_merek,
-                "jenis_kemasan": sertifikat_pl.jenis_kemasan,
-                "berat_bersih": sertifikat_pl.berat_bersih,
-                "unit_penanganan": " ",
-                "nama_unit_penanganan": unit_produksi.nama_unit,
-                "alamat_unit_penanganan": unit_produksi.alamat_unit,
-                "status_kepemilikan": unit_produksi.status_kepemilikan,
-                "sppb_psat": " ",
-                "no_sppb_psat": unit_produksi.sppb_psat_nomor,
-                "level_sppb_psat": unit_produksi.level_sppb_psat,
-                "berlaku_sampai_sppb_psat": unit_produksi.sppb_psat_masa_berlaku,
-                "ruang_lingkup_sppb_psat": unit_produksi.ruang_lingkup
-                    //               "kelas_mutu_sppb_psat": sertifikat_pl.kelas_mutu,
-                    //               "jenis_klaim_sppb_psat": sertifikat_pl.jenis_klaim,
-                    //                "desain_kemasan_sppb_psat": sertifikat_pl.desain_tabel_dan_kemasan
+                    "pbumku": "pbumku",
+                    "nama_psat": "nama_psat",
+                    "jenis_psat": sertifikat_pl.jenis_psat,
+                    "nama_dagang": sertifikat_pl.nama_dagang,
+                    "izin_psat_pl": " ",
+                    "no_izin_psat_pl": def.no_izin_psat_pl,
+                    "berlaku_sampai": berlaku_sampai,
+                    "nama_latin": sertifikat_pl.nama_latin,
+                    "negara_asal": sertifikat_pl.negara_asal,
+                    "nama_merk": sertifikat_pl.nama_merek,
+                    "jenis_kemasan": sertifikat_pl.jenis_kemasan,
+                    "berat_bersih": sertifikat_pl.berat_bersih,
+                    "unit_penanganan": " ",
+                    "nama_unit_penanganan": unit_produksi.nama_unit,
+                    "alamat_unit_penanganan": unit_produksi.alamat_unit,
+                    "status_kepemilikan": unit_produksi.status_kepemilikan,
+                    "sppb_psat": " ",
+                    "no_sppb_psat": unit_produksi.sppb_psat_nomor,
+                    "level_sppb_psat": unit_produksi.level_sppb_psat,
+                    "berlaku_sampai_sppb_psat": unit_produksi.sppb_psat_masa_berlaku,
+                    "ruang_lingkup_sppb_psat": unit_produksi.ruang_lingkup
+                        //               "kelas_mutu_sppb_psat": sertifikat_pl.kelas_mutu,
+                        //               "jenis_klaim_sppb_psat": sertifikat_pl.jenis_klaim,
+                        //                "desain_kemasan_sppb_psat": sertifikat_pl.desain_tabel_dan_kemasan
 
-            }
-            let filename = await 'sertifikat/psat-pl/' + sertifikat_pl.id_pengguna + '-' + sertifikat_pl.id_pengajuan + '-' + def.no_izin_psat_pl + '.pdf'
-            const templatePath = Path.resolve('models', 'template_pdf', 'OSS_PL.html')
+                }
+                let filename = await 'sertifikat/psat-pl/' + sertifikat_pl.id_pengguna + '-' + sertifikat_pl.id_pengajuan + '-' + def.no_izin_psat_pl + '.pdf'
+                const templatePath = Path.resolve('models', 'template_pdf', 'OSS_PL.html')
 
-            const content = await ReadFile(templatePath, 'utf8')
-                // compile and render the template with handlebars
-            const template = Handlebars.compile(content)
+                const content = await ReadFile(templatePath, 'utf8')
+                    // compile and render the template with handlebars
+                const template = Handlebars.compile(content)
 
-            const pdf = await generatePdf.pdf(template(data), filename);
+                const pdf = await generatePdf.pdf(template(data), filename);
 
-            let path_sertifikat = url + filename
+                let path_sertifikat = url + filename
 
-            let data_pengajuan = [sertifikat_pl.id_pengajuan, sertifikat_pl.id_pengguna, sertifikat_pl.status_pengajuan, path_sertifikat, date, def.no_izin_psat_pl, berlaku_sampai];
-            let pengajuan = await pool.query(
-                'UPDATE ' + db_pengajuan_izin_edar +
-                ' SET (final_sertifikat, update, nomor_izin_edar, berlaku_sampai) = ($4, $5, $6, $7) WHERE id=$1 AND id_pengguna=$2 AND status_pengajuan=$3 ' +
-                'RETURNING id, id_pengguna, status_pengajuan, status_proses, final_sertifikat', data_pengajuan);
+                let data_pengajuan = [sertifikat_pl.id_pengajuan, sertifikat_pl.id_pengguna, sertifikat_pl.status_pengajuan, path_sertifikat, date, def.no_izin_psat_pl, berlaku_sampai];
+                let pengajuan = await pool.query(
+                    'UPDATE ' + db_pengajuan_izin_edar +
+                    ' SET (final_sertifikat, update, nomor_izin_edar, expire_sertifikat) = ($4, $5, $6, $7) WHERE id=$1 AND id_pengguna=$2 AND status_pengajuan=$3 ' +
+                    'RETURNING id, id_pengguna, status_pengajuan, status_proses, final_sertifikat', data_pengajuan);
 
-            // res.set("Content-Type", "application/pdf");
-            var pdfsToMerge = []
-            pdfsToMerge.push(fs.readFileSync(filename))
-                //	    pdfsToMerge.push(fs.readFileSync(filename))
+                // res.set("Content-Type", "application/pdf");
+                var pdfsToMerge = []
+                pdfsToMerge.push(fs.readFileSync(filename))
+                    //	    pdfsToMerge.push(fs.readFileSync(filename))
 
-            if (sertifikat_pl.kelas_mutu != null) {
-                console.log("hai")
-                pdfsToMerge.push(fs.readFileSync(sertifikat_pl.kelas_mutu))
-            }
+                if (sertifikat_pl.kelas_mutu != null) {
+                    console.log("hai")
+                    pdfsToMerge.push(fs.readFileSync(sertifikat_pl.kelas_mutu))
+                }
 
-            if (sertifikat_pl.diagram_alir_psat_luar_negri != null) {
-                console.log("hai")
-                pdfsToMerge.push(fs.readFileSync(sertifikat_pl.diagram_alir_psat_luar_negri))
-            }
-            console.log(sertifikat_pl.diagram_alir_psat_luar_negri)
+                if (sertifikat_pl.diagram_alir_psat_luar_negri != null) {
+                    console.log("hai")
+                    pdfsToMerge.push(fs.readFileSync(sertifikat_pl.diagram_alir_psat_luar_negri))
+                }
+                console.log(sertifikat_pl.diagram_alir_psat_luar_negri)
 
-            if (sertifikat_pl.desain_tabel_dan_kemasan != null) {
-                pdfsToMerge.push(fs.readFileSync(sertifikat_pl.desain_tabel_dan_kemasan))
-            }
+                if (sertifikat_pl.desain_tabel_dan_kemasan != null) {
+                    pdfsToMerge.push(fs.readFileSync(sertifikat_pl.desain_tabel_dan_kemasan))
+                }
 
 
-            const mergedPdf = await PDFDocument.create();
-            for (const pdfBytes of pdfsToMerge) {
-                const pdf = await PDFDocument.load(pdfBytes);
-                const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-                copiedPages.forEach((page) => {
-                    mergedPdf.addPage(page);
-                });
-            }
+                const mergedPdf = await PDFDocument.create();
+                for (const pdfBytes of pdfsToMerge) {
+                    const pdf = await PDFDocument.load(pdfBytes);
+                    const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+                    copiedPages.forEach((page) => {
+                        mergedPdf.addPage(page);
+                    });
+                }
 
-            const buf = await mergedPdf.save(); // Uint8Array
+                const buf = await mergedPdf.save(); // Uint8Array
 
-            let path = filename;
-            fs.open(path, 'w', function(err, fd) {
-                fs.write(fd, buf, 0, buf.length, null, function(err) {
-                    fs.close(fd, function() {
-                        console.log('wrote the file successfully');
+                let path = filename;
+                fs.open(path, 'w', function(err, fd) {
+                    fs.write(fd, buf, 0, buf.length, null, function(err) {
+                        fs.close(fd, function() {
+                            console.log('wrote the file successfully');
+                        });
                     });
                 });
-            });
-            res.status(200).json({
-                message: "Sertifikat PSAT PL",
-                path: path_sertifikat,
-                data: sertifikat_pl,
-                pengajuan: unit_produksi
-            });
+                res.status(200).json({
+                    message: "Sertifikat PSAT PL",
+                    path: path_sertifikat,
+                    data: sertifikat_pl,
+                    pengajuan: unit_produksi
+                });
 
-            // } catch (e) {
-            //     next(e.detail || e);
-            // }
+            } catch (e) {
+                next(e.detail || e);
+            }
         };
         let fallback = (err) => {
             next(err);
