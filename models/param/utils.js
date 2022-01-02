55c8404e9d4d111d6fac57e1fe7c785ccc2b3266
Denanda Hendra <denandahp@gmail.com>
2022-01-02 09:18:55 +0700

    fix date

--- models/param/utils.js
+++ models/param/utils.js
@@ -16,10 +16,10 @@ exports.limit_time = (limit)=> {
 
 exports.date_now = ()=> {
     var d = new Date(Date.now());
-    d.toLocaleString('en-GB', { timeZone: 'Asia/Jakarta' });
     var dd = d.getDate();var mm = d.getMonth() + 1;var y = d.getFullYear();var hour = d.getHours();var minute = d.getMinutes();var second = d.getSeconds();
     var FormattedDate = y + '-'+ mm + '-'+ dd + ' ' + hour + ':'+ minute + ':'+ second;
-    return String(FormattedDate);
+    console.log(FormattedDate)
+    return FormattedDate;
 }
 
 exports.time_format = ()=> {
