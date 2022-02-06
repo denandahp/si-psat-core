68976d54165d7613900849c15dae0f25edc04a30
Denanda Hendra <denandahp@gmail.com>
2022-02-06 13:12:28 +0700

    add expired

--- controllers/oss.js
+++ controllers/oss.js
@@ -26,8 +26,10 @@ class OSSController {
         let callback = async() => {
             try {
                 let data =  req.query;
+                let expired_token =  req.validateToken.OSS_result.expired_aToken;
+                console.log(expired_token)
                 let access_token = req.headers.authorization.split('Bearer ')[1];
-                let detail = await oss.pelaku_usaha(data, access_token);
+                let detail = await oss.pelaku_usaha(data, access_token, expired_token);
 
                 if (detail.status == '400') { res.status(400).json({ detail }); } else { res.status(200).json({ response: 200, data: detail }); }
             } catch (e) {
--- models/oss.js
+++ models/oss.js
@@ -65,7 +65,7 @@ class OSSModel {
         };
     }
 
-    async pelaku_usaha(data, access_token) {
+    async pelaku_usaha(data, access_token, expired_token) {
         try {
             let response = {};
             let token = await oss_param.generate_token('userinfo')
@@ -113,6 +113,7 @@ class OSSModel {
                     '= ($1, $2, $3) WHERE info_kepemilikan=$1 RETURNING ' +
                     'id, info_kepemilikan, role, created', [query_data.rows[0].id, 'PELAKU_USAHA', date]);
             }
+            response.expired_token = expired_token
             response.user = pengguna.rows[0];
             response.user_detail = query_data.rows[0];
             debug('get %o', response);
