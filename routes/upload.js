20a94fa946ca5e84a39af59dd415eefb716dbdba
Denanda Hendra <denandahp@gmail.com>
2022-09-24 18:54:20 +0700

    fix auth

--- routes/upload.js
+++ routes/upload.js
@@ -3,8 +3,8 @@ const upload = require('../controllers/utils/upload_file.js');
 const auth = require('../middleware/auth.js');
 
 
-Router.post('/file', auth, upload.upload_file)
-      .get('/view_pdf', auth, upload.view_pdf)
-      .get('/display_image', auth, upload.display_image)
+Router.post('/file', upload.upload_file)
+      .get('/view_pdf', upload.view_pdf)
+      .get('/display_image', upload.display_image)
 
 module.exports = Router;
