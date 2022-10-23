ec112e942fbe4fb6ce0bcf46e43af9da494e9dec
Denanda Hendra <denandahp@gmail.com>
2022-10-23 10:37:51 +0700

    template excel

--- okkp/dashboard/routes.js
+++ okkp/dashboard/routes.js
@@ -5,5 +5,6 @@ const auth = require('../../middleware/auth.js')
 Router.get('/statistik_registrasi', controllers.statistik_registrasi)
       .get('/registrasi_by_provinsi', controllers.registrasi_by_provinsi)
       .get('/komoditas', controllers.komoditas)
+      .get('/statistik_uji_lab', controllers.statistik_uji_lab)
 
 module.exports = Router;
