f814c5a653a30d99b4a49fb517451cbb5ed7d210
Denanda Hendra <denandahp@gmail.com>
2022-11-25 16:51:09 +0700

    fix update uji lab

--- okkp/uji_lab/models.js
+++ okkp/uji_lab/models.js
@@ -1,6 +1,6 @@
 const core = require('../core.js')
 const pool = require('../../libs/okkp_db.js');
-const utils = require('./utils.js')
+const {SerializeUjiLab} = require('./utils.js')
 const utils_core = require('../../okkp/utils.js')
 var format = require('pg-format');
 
@@ -13,26 +13,23 @@ class OkkpUjiLabController {
     async create_uji_lab(data, user) {
         try {
             let uji_lab;
-            let {key, value} = await utils.serialize_uji_lab(data, user, 'created')
-            await pool.query('BEGIN');
-            uji_lab = await pool.query(format('INSERT INTO ' + db_uji_lab + ` (${key}) VALUES %L RETURNING *`, value));
-            await pool.query('COMMIT');
+            let {key, value} = new SerializeUjiLab(data, user, 'created').save()
+            let query = format('INSERT INTO ' + db_uji_lab + ` (${key}) VALUES %L RETURNING *`, value)
+            uji_lab = await pool.query(query);
             return { status: '200', data: uji_lab.rows };
         } catch (ex) {
-            await pool.query('ROLLBACK');
             return { status: '400', Error: "" + ex };
         };
     }
 
     async update_uji_lab(data, user) {
         try {
-            let {key, value} = utils.serialize_uji_lab(data, user, 'updated')
-            await pool.query('BEGIN');
-            let uji_lab = await pool.query(format('UPDATE ' + db_uji_lab + ` SET (${key}) = %L WHERE id = ${data.id} RETURNING *`, value));
-            await pool.query('COMMIT');
+            let {key, value} = new SerializeUjiLab(data, user, 'updated').save()
+            let query = format('UPDATE ' + db_uji_lab + ` SET (${key}) = (%L) WHERE id = ${data.id} RETURNING *`, value)
+            console.log(query)
+            let uji_lab = await pool.query(query);
             return { status: '200', data: uji_lab.rows[0] };
         } catch (ex) {
-            await pool.query('ROLLBACK');
             return { status: '400', Error: "" + ex };
         };
     }
--- okkp/uji_lab/utils.js
+++ okkp/uji_lab/utils.js
@@ -6,29 +6,74 @@ var date = utils.date_now();
 const schema_regis = 'register';
 
 
-exports.serialize_uji_lab = (data, user, process) => {
-    let field_db = [
-        "jenis_uji_lab_id", "user_id", "lembaga", "tanggal", "lokasi_sampel", "komoditas_id", "komoditas_tambahan",
-        "parameter", "hasil_uji", "standar", "status_id", "referensi_bmr", "metode_uji", "note", "updated_at", 
-        "modified_by", "provinsi_id"
-    ]
+class SerializeUjiLab{
 
-    if(process == 'created'){
-        field_db.push("created_at", "created_by")
+    constructor(data, user, process){
+        this.data = data;
+        this.user = user;
+        this.process = process
+        
     }
-    let value = data.multiple_parameter.map((val) => {
-        let lembaga = (data.lembaga) ? data.lembaga : user.duduk_lembaga
-        value_list =  [
-            data.jenis_uji_lab_id, user.id, lembaga, data.tanggal, data.lokasi_sampel, data.komoditas_id, data.komoditas_tambahan,
-            val.parameter, val.hasil_uji, val.standar, data.status_id, data.referensi_bmr, data.metode_uji, data.note, date,
-            user.email, data.provinsi_id
-        ]
-        if(process == 'created'){
-            value_list.push(date, user.email)
-        }
-        return value_list
-    });
 
-    let key = field_db.toString()
-    return {key, value}
+    create() {
+        let value = this.data.multiple_parameter.map((val) => {
+            let lembaga = (this.data.lembaga) ? this.data.lembaga : this.user.duduk_lembaga
+            let value_list = [
+                this.data.jenis_uji_lab_id, this.user.id, lembaga, this.data.tanggal, this.data.lokasi_sampel, this.data.komoditas_id, this.data.komoditas_tambahan,
+                val.parameter, val.hasil_uji, val.standar, this.data.status_id, this.data.referensi_bmr, this.data.metode_uji, this.data.note, date,
+                this.user.email, this.data.provinsi_id, date, this.user.email
+            ]
+
+            return value_list
+        });
+    
+        return value
+    }
+
+    update() {
+        let serialize = {
+            "jenis_uji_lab_id": this.data.jenis_uji_lab_id, 
+            "user_id": this.user.id, 
+            "lembaga": (this.data.lembaga) ? this.data.lembaga : this.user.duduk_lembaga, 
+            "tanggal": utils.date_format(this.data.tanggal), 
+            "lokasi_sampel": this.data.lokasi_sampel, 
+            "komoditas_id": this.data.komoditas_id, 
+            "komoditas_tambahan": this.data.komoditas_tambahan,
+            "parameter": this.data.parameter, 
+            "hasil_uji": this.data.hasil_uji, 
+            "standar": this.data.standar, 
+            "status_id": this.data.status_id, 
+            "referensi_bmr": this.data.referensi_bmr, 
+            "metode_uji": this.data.metode_uji, 
+            "note": this.data.note, 
+            "updated_at": date, 
+            "modified_by": this.user.email, 
+            "provinsi_id": this.data.provinsi_id
+        }
+        let value = Object.values(serialize)
+        return value
+    }
+
+    save(){
+        let value;
+        let field_db = [
+            "jenis_uji_lab_id", "user_id", "lembaga", "tanggal", "lokasi_sampel", "komoditas_id", "komoditas_tambahan",
+            "parameter", "hasil_uji", "standar", "status_id", "referensi_bmr", "metode_uji", "note", "updated_at", 
+            "modified_by", "provinsi_id"
+        ]
+    
+        if(this.process == 'created'){
+            field_db.push("created_at", "created_by")
+            value = this.create()
+        }else{
+            value = this.update()
+        }
+
+        let key = field_db.toString()
+        return {key, value}
+    }
+
 }
+
+module.exports = {SerializeUjiLab}
+
